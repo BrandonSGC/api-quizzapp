26a47ad96aab172340191a88927c6d4c29eb26bc
BrandonSGC <brangmz20@gmail.com>
2024-04-25 12:27:11 -0600

    Get user by id

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,10 +1,23 @@
 import Users from "../models/Users.js";
 
-export const getAllUsers = async (req, res) => {
+export const getUserById = async (req, res) => {
   try {
-    const users = await Users.findAll();
-    res.send(users);
+    // Get user id.
+    const { id } = req.params;
+
+    //TODO: Validate id.
+
+    // Validate we have a user.
+    const user = await Users.findByPk(id);
+
+    if (user) {
+      res.status(200).json(user);
+      return;
+    }
+
+    res.status(200).json({ message: "We couldn't found the user." });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ message: "An error has occured in the server." });
   }
 };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import { getUserById } from '../controllers/users.controller.js';
 
 const usersRouter = Router();
 
-usersRouter.get('/',(req, res) => {
-  res.send('Hi user!');
-})
+usersRouter.get('/:id', getUserById);
 
 export default usersRouter;
