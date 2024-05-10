b46839aae767bc1f0689adb27de9f3e16998835c
BrandonSGC <brangmz20@gmail.com>
2024-05-10 16:16:47 -0600

    Format code

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,19 @@
-import { Router } from 'express';
-import { getUserById, createUser, login, logout, profile } from '../controllers/users.controller.js';
-import { authRequired } from '../middlewares/validateToken.js';
+import { Router } from "express";
+import {
+  getUserById,
+  createUser,
+  login,
+  logout,
+  profile,
+} from "../controllers/users.controller.js";
+import { authRequired } from "../middlewares/validateToken.js";
 
 const router = Router();
 
-router.post('/', createUser);
-router.get('/:id', getUserById);
-router.post('/login', login);
-router.post('/logout', logout);
-router.get('/', authRequired, profile);
+router.post("/", createUser);
+router.get("/:id", getUserById);
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/", authRequired, profile);
 
 export default router;
